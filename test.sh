8b278d76f9d3a81c1b55ae2575405ef2bebcd4e8
Saad Altabari <saadtabari@gmail.com>
2021-11-02 21:22:45 -0700

    remove commented out code from test.sh file

--- test.sh
+++ test.sh
@@ -3,7 +3,4 @@ docker-compose -f docker-compose.test.yml up -d --build
 
 docker-compose -f docker-compose.test.yml run web-server pytest
 
-#source envs/test.env
-#docker-compose -f docker-compose.test.yml run mongodb mongo --username=$MONGO_DB_USER --password=$MONGO_DB_PASSWORD --authenticationDatabase=$MONGO_DB_AUTH_SOURCE test --eval "printjson(db.dropDatabase())"
-
 docker-compose -f docker-compose.test.yml down

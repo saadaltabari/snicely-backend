1bd240a5cb7578471b69ad22fcf2ed23fbeb6d6c
Saad Altabari <saadtabari@gmail.com>
2021-11-13 22:59:32 -0500

    update default database entries

--- initdb.d/user_text.js
+++ initdb.d/user_text.js
@@ -3,7 +3,7 @@ db.usertext.insertMany([
   {
     "user_id": "x1z21",
     "user_text": "Snicely is an immature applicatoin!",
-    "toxicity_score": 0.5,
+    "toxicity_score": 0.71,
     "date": "2021-10-25T07:33:04+00:00"
   },
   {
@@ -14,8 +14,8 @@ db.usertext.insertMany([
   },
   {
     "user_id": "ab123",
-    "user_text": "You guys are doing a great job",
-    "toxicity_score": 0.05,
+    "user_text": "The most horrible part is that she is just plain mean and crazy. I definitely don't like her. Who watches lame TV shows and crappy movies anyway?",
+    "toxicity_score": 0.78,
     "date": "2021-10-12T16:53:43+00:00"
   }
 ]);

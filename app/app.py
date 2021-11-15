ee3100abc06d0e0040cbd6ad4d571baf43349d52
Saad Altabari <saadtabari@gmail.com>
2021-11-15 12:31:55 -0500

    only save text if negative

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from flask import (
     redirect,
     jsonify
 )
+from flask_cors import CORS, cross_origin
 
 # from decorators import authorize_admin_users_only
 from handlers import PerspectiveAPIToxicityHandler
@@ -18,6 +19,7 @@ from settings import SETTINGS
 app = Flask(__name__)
 app.config.from_mapping(SETTINGS)
 db.init_app(app)
+CORS(app)
 
 
 @app.route('/history', methods=['GET'])
@@ -39,23 +41,26 @@ def user_flagged_text_history():
 
 
 @app.route('/validate', methods=['POST'])
+@cross_origin()
 def validate_user_text():
     """
     text validation http
     """
     user_data = request.form
     user_text = user_data.get('text')
+    print(f"user text is {user_text}")
     handler = PerspectiveAPIToxicityHandler(user_text=user_text)
     is_toxic, toxic_text = handler.evaluate()
     request_time = datetime.now()
 
-    for text in toxic_text:
-        snicely_user_text = UserText()
-        snicely_user_text.user_text = text['text']
-        snicely_user_text.user_id = "123"# default user_id for now
-        snicely_user_text.toxicity_score = text['toxicity_score']
-        snicely_user_text.date = request_time
-        snicely_user_text.save()
+    if is_toxic:
+        for text in toxic_text:
+            snicely_user_text = UserText()
+            snicely_user_text.user_text = text['text']
+            snicely_user_text.user_id = "123"# default user_id for now
+            snicely_user_text.toxicity_score = text['toxicity_score']
+            snicely_user_text.date = request_time
+            snicely_user_text.save()
 
     return {
         'is_toxic': is_toxic,
